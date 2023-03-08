39814a80343adbcf0c3eabc95173d3a5861cdca6
Mudusar204 <hafizmudusar37@gmail.com>
2023-03-08 12:17:08 +0500

    2nd commit in enver app

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Image from 'next/image'
 const About = () => {
     return (
         <div className='' id='about'>
@@ -7,12 +7,12 @@ const About = () => {
                 <h1 className='text-white text-4xl font-bold'>Contact us for the service
                     <br />
                     you want to use</h1>
-                    <img src={'/add.png'} className='absolute top-10 right-10' alt="" />
-                    <img src={'/circle.png'} className='absolute bottom-10 ' alt="" />
+                    <Image src={'/add.png'} className='absolute top-10 right-10' alt="" />
+                    <Image src={'/circle.png'} className='absolute bottom-10 ' alt="" />
                 <button className='rounded-md bg-[#5454D4] border-gray-600 border px-8 py-2  text-white font-bold'>Contact us</button>
             </div>
             <footer className='flex justify-between max-lg:flex-col max-lg:gap-10 mx-[7%] items-center pt-5 pb-10 '>
-                <div><img src={'/logo.png'} width={100} height={100} alt="nav logo" /></div>
+                <div><Image src={'/logo.png'} width={100} height={100} alt="nav logo" /></div>
                 <div><ul className='flex  gap-6 max-sm:flex-col items-center text-[#D9D9D9] '>
                     <li className='hover:text-[#5454D4] text-[#D9D9D9]'><button>Support</button>  </li>
                     <li className='hover:text-[#5454D4] text-[#D9D9D9]'><button>Privacy Policy</button></li>
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../../public/logo.png'
 import { useState } from 'react'
+import Image from 'next/image'
 const Navbar = () => {
   function handleLinkClick(e:string) {
     
@@ -23,7 +24,7 @@ const Navbar = () => {
   return (
     <div className='text-white'>
       <nav className='flex justify-between mx-[7%] items-center max-md:items-start py-5 '>
-        <div><img src={'/logo.png'} width={100} height={100} alt="nav logo" /></div>
+        <div><Image src={'/logo.png'} width={100} height={100} alt="nav logo" /></div>
         <div>
           <div>
           <ul className={`  max-md:${loader ? 'block' : 'hidden'
@@ -38,7 +39,7 @@ const Navbar = () => {
           <button className='rounded-md border-gray-600 border px-8 py-2 bg-transparent text-[#D9D9D9]'>Contact us</button></div>
         <div className='md:hidden max-md:block ml-4 mt-3'>
           <button onClick={menuFunction}>
-            <img src={"/menuIcon.png"} alt="" /></button></div>
+            <Image src={"/menuIcon.png"} alt="" /></button></div>
 
       </nav>
     </div>
